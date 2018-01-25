701a9a826a0a2879863c8d3f5c0b24f165941bd0
tosektosek <kamil.tos@zoho.eu>
2018-01-25 18:05:30 +0100

    Added new products and ProductRepository

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.packt.webstore.controller;
 
 import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,16 @@ import java.math.BigDecimal;
 @Controller
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @RequestMapping("/products")
     public String list(Model model) {
-        Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
-        iphone.setDescription("Apple iPhone 5s, smartfon z 4-calowym wyświetlaczem o rozdzielczości 640x1136 oraz 8-megapikselowym aparatem");
-        iphone.setCategory("Smart Phone");
-        iphone.setManufacturer("Apple");
-        iphone.setUnitsInStock(1000);
-        model.addAttribute("product", iphone);
-
+        model.addAttribute("products", productRepository.getAllProducts());
         return "products";
     }
 }
---
+++ src/main/java/com/packt/webstore/domain/repository/InMemoryProductRepository.java
@@ -0,0 +1,47 @@
+package com.packt.webstore.domain.repository;
+
+import com.packt.webstore.domain.Product;
+import org.springframework.stereotype.Repository;
+import sun.java2d.cmm.Profile;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kamil
+ */
+@Repository
+public class InMemoryProductRepository implements ProductRepository {
+
+    private List<Product> listOfProducts = new ArrayList<>();
+
+    public InMemoryProductRepository() {
+
+        Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
+        iphone.setDescription("Apple iPhone 5s, smartfon z 4-calowym wyświetlaczem o rozdzielczości 640x1136 oraz 8-megapikselowym aparatem");
+        iphone.setCategory("Smart Phone");
+        iphone.setManufacturer("Apple");
+        iphone.setUnitsInStock(1000);
+
+        Product laptop_dell = new Product("P1235", "Dell Inspiron", new BigDecimal(700));
+        laptop_dell.setDescription("Dell Inspiron, 14-calowy laptop (czarny) z procesorem Intel Core 7. generacji");
+        laptop_dell.setCategory("Laptop");
+        laptop_dell.setManufacturer("Dell");
+        laptop_dell.setUnitsInStock(1000);
+
+        Product tablet_Nexus = new Product("P1236", "Nexus 7", new BigDecimal(300));
+        tablet_Nexus.setDescription("Google Nexus 7 jest najlżejszym 7-caolwym tabletem z 4-rdzeniowym procesorem Qualcomm Snapdragon® S4 Pro");
+        tablet_Nexus.setCategory("Tablet");
+        tablet_Nexus.setManufacturer("Google");
+        tablet_Nexus.setUnitsInStock(1000);
+
+        listOfProducts.add(iphone);
+        listOfProducts.add(laptop_dell);
+        listOfProducts.add(tablet_Nexus);
+
+    }
+    public List<Product> getAllProducts() {
+        return listOfProducts;
+    }
+}
